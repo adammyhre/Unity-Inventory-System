20238e11f4c769e904e3d76962ba07df69c22f83
adammyhre <38876398+adammyhre@users.noreply.github.com>
2024-03-03 05:47:55 -0800

    Video 5: Addressable Scenes

--- Assets/_Project/Scripts/SceneManagement/SceneGroupManager.cs
+++ Assets/_Project/Scripts/SceneManagement/SceneGroupManager.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Eflatun.SceneReference;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
 
 namespace Systems.SceneManagement {
@@ -10,6 +14,8 @@ namespace Systems.SceneManagement {
         public event Action<string> OnSceneLoaded = delegate { };
         public event Action<string> OnSceneUnloaded = delegate { };
         public event Action OnSceneGroupLoaded = delegate { };
+        
+        readonly AsyncOperationHandleGroup handleGroup = new AsyncOperationHandleGroup(10);
         
         SceneGroup ActiveSceneGroup;
         
@@ -32,17 +38,24 @@ namespace Systems.SceneManagement {
             for (var i = 0; i < totalScenesToLoad; i++) {
                 var sceneData = group.Scenes[i];
                 if (reloadDupScenes == false && loadedScenes.Contains(sceneData.Name)) continue;
-
-                var operation = SceneManager.LoadSceneAsync(sceneData.Reference.Path, LoadSceneMode.Additive);
-
-                operationGroup.Operations.Add(operation);
-
+                
+                if (sceneData.Reference.State == SceneReferenceState.Regular)
+                {
+                    var operation = SceneManager.LoadSceneAsync(sceneData.Reference.Path, LoadSceneMode.Additive);
+                    operationGroup.Operations.Add(operation);
+                }
+                else if (sceneData.Reference.State == SceneReferenceState.Addressable)
+                {
+                    var sceneHandle = Addressables.LoadSceneAsync(sceneData.Reference.Path, LoadSceneMode.Additive);
+                    handleGroup.Handles.Add(sceneHandle);
+                }
+                
                 OnSceneLoaded.Invoke(sceneData.Name);
             }
             
             // Wait until all AsyncOperations in the group are done
-            while (!operationGroup.IsDone) {
-                progress?.Report(operationGroup.Progress);
+            while (!operationGroup.IsDone || !handleGroup.IsDone) {
+                progress?.Report((operationGroup.Progress + handleGroup.Progress) / 2);
                 await Task.Delay(100);
             }
 
@@ -67,6 +80,8 @@ namespace Systems.SceneManagement {
                 
                 var sceneName = sceneAt.name;
                 if (sceneName.Equals(activeScene) || sceneName == "Bootstrapper") continue;
+                if (handleGroup.Handles.Any(h => h.IsValid() && h.Result.Scene.name == sceneName)) continue;
+                
                 scenes.Add(sceneName);
             }
             
@@ -81,6 +96,13 @@ namespace Systems.SceneManagement {
 
                 OnSceneUnloaded.Invoke(scene);
             }
+            
+            foreach (var handle in handleGroup.Handles) {
+                if (handle.IsValid()) {
+                    Addressables.UnloadSceneAsync(handle);
+                }
+            }
+            handleGroup.Handles.Clear();
 
             // Wait until all AsyncOperations in the group are done
             while (!operationGroup.IsDone) {
@@ -101,6 +123,16 @@ namespace Systems.SceneManagement {
         public AsyncOperationGroup(int initialCapacity) {
             Operations = new List<AsyncOperation>(initialCapacity);
         }
+    }
+
+    public readonly struct AsyncOperationHandleGroup {
+        public readonly List<AsyncOperationHandle<SceneInstance>> Handles;
         
+        public float Progress => Handles.Count == 0 ? 0 : Handles.Average(h => h.PercentComplete);
+        public bool IsDone => Handles.Count == 0 || Handles.All(o => o.IsDone);
+
+        public AsyncOperationHandleGroup(int initialCapacity) {
+            Handles = new List<AsyncOperationHandle<SceneInstance>>(initialCapacity);
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,11 +2,22 @@
 
 ![Inventory1](https://github.com/adammyhre/Unity-Inventory-System/assets/38876398/9cd5e87d-c053-4997-81a3-b162491b87c7)
 
-Welcome to the Unity Inventory System built with UI Toolkit!  
-This project compliments our 2024 YouTube Series!  
+## Overview
+
+Welcome to the Unity Inventory System built with UI Toolkit! This project compliments our 2024 YouTube video series - check it out!
+
+### Requirements
+
+- **Unity 2023.2 or above**
+- Addressables 1.19.19 or above
+- [Eflatun.SceneReference](https://github.com/starikcetin/Eflatun.SceneReference) open source library by S. Tarık Çetin
+
+## YouTube
 
 - [Episode 1: Building Prodedural UI with Drag and Drop](https://youtu.be/MOiXqKFHAIs)
 - [Episode 2: Logic, Data and Data Binding](https://youtu.be/g2a4ZK8cEso)
 - [Episode 3: Save/Load Data Persistence](https://youtu.be/z1sMhGIgfoo)
+- [Episode 4: Awaitable Async Multi-Scene Loading](https://youtu.be/JFP-cCFID7o)
+- Episode 5: Addressable Scenes coming soon...
 
 Join us on the **git-amend** [YouTube channel](https://www.youtube.com/@git-amend?sub_confirmation=1) for weekly updates and other cool stuff.
