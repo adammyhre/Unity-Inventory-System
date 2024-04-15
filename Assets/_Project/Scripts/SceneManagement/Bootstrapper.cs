f87a2f974c89046f8d4d99a688f6d29fd1e4e6ae
adammyhre <38876398+adammyhre@users.noreply.github.com>
2024-04-15 16:59:12 -0700

    Update Bootstrapper.cs

--- Assets/_Project/Scripts/SceneManagement/Bootstrapper.cs
+++ Assets/_Project/Scripts/SceneManagement/Bootstrapper.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
 
 public class Bootstrapper : PersistentSingleton<Bootstrapper> {
+    // NOTE: This script is intended to be placed in your first scene included in the build settings.
+    static readonly int sceneIndex = 0;
+    
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    static async void Init() {
+    static void Init() {
         Debug.Log("Bootstrapper...");
-        await SceneManager.LoadSceneAsync("Bootstrapper", LoadSceneMode.Single);
+#if UNITY_EDITOR
+        // Set the bootstrapper scene to be the play mode start scene when running in the editor
+        // This will cause the bootstrapper scene to be loaded first (and only once) when entering
+        // play mode from the Unity Editor, regardless of which scene is currently active.
+        EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[sceneIndex].path);
+#endif
     }
 }
